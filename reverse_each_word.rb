2a5bb667ba4a1420eaa4e66932e82d0c44d03ede
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-11 09:57:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,10 @@
   def reverse_each_word (sentence)
 
     reversed_sentence = []
-    words = sentence.split(" ")
-    
+    words = sentence.split(" ") #converts the input sentence into an array of words
 
-    words.collect do |sentence|
+
+    words.collect do |sentence| #collect each word in the sentence
 
       reversed_sentence.push(sentence.reverse)
       #reversed_sentence.push(sentence_blocks[i])
